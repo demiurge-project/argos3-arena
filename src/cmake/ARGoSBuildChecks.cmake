17e37717bf05daddd2b1136970e0e9c2bcd24071
mkegelei <mkegelei@ulb.ac.be>
2020-10-05 17:29:13 +0200

    update Lua requirements

--- src/cmake/ARGoSBuildChecks.cmake
+++ src/cmake/ARGoSBuildChecks.cmake
@@ -17,11 +17,11 @@ if(ARGOS_BUILD_FOR_SIMULATOR)
 endif(ARGOS_BUILD_FOR_SIMULATOR)
 
 #
-# Check for Lua 5.2
+# Check for Lua 5.3
 #
-find_package(Lua52)
-if(LUA52_FOUND)
+find_package(Lua53)
+if(LUA53_FOUND)
   set(ARGOS_WITH_LUA ON)
   include_directories(${LUA_INCLUDE_DIR})
-endif(LUA52_FOUND)
+endif(LUA53_FOUND)
 
